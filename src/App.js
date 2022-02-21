7acff4b520cba2e60e5a4e758886921604938ef7
Иван Гаврильков <ivan.gavrilkov@gmail.com>
2022-02-21 23:45:45 +0200

    comlete lab1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [counter, setCounter] = useState(20);
+  const [buttonsState, setButtonsState] = useState(true);
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  }
+  const decreaseCounter = () => {
+    setCounter(counter - 1);
+  }
+  const setDefState = () => {
+    setCounter(20);
+    setButtonsState(true);
+  }
+
+  useEffect(() => {
+    const keyHandler = event => {
+      if (event.key === '+' || event.key === '=') {
+        setCounter(counter + 1);
+      }
+      else if (event.key === '-' || event.key === '_') {
+        setCounter(counter - 1);
+      }
+    }
+    if (!buttonsState) {
+      document.addEventListener('keydown', keyHandler)
+    }
+    return () => {
+      document.removeEventListener('keydown', keyHandler)
+    }
+  })
   return (
     <div className="App">
-
-    </div>
+      <p>{counter}</p>
+      {
+        buttonsState ?
+          <div className="buttons-container">
+            <button
+              onClick={increaseCounter}
+              style={{ background: '#5cb85c' }}
+            >
+              +
+            </button>
+            <button
+              onClick={decreaseCounter}
+              style={{ background: '#f0ad4e' }}
+            >
+              -
+            </button>
+          </div>
+          :
+          null
+      }
+      <ul className='list-box'>
+        <li><p className='App-link' onClick={() => { setButtonsState(true) }}>Показать</p></li>
+        <li><p className='App-link' onClick={() => { setButtonsState(false) }}>Скрыть</p></li>
+        <li><p className='App-link' onClick={setDefState}>Очистить</p></li>
+      </ul>
+      <button
+        className='kill-btn'
+        style={{ background: '#d9534f' }}
+        onClick={() => window.close()}
+      >
+        Close
+      </button>
+    </div >
   );
 }
 
